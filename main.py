b07056bda027b41d2c7dcbe63da872ec165bb4c1
DUCH18 <felipeduchen2@gmail.com>
2026-02-25 00:46:08 -0300

    estrutura inicial mais extract da api de filmes de teste

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from etllocal-films!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/extract_data_films.py
@@ -0,0 +1,37 @@
+import requests
+import json
+from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+API_KEY = os.getenv('API_KEY')
+
+# url = 'https://api.themoviedb.org/3/movie/11?api_key=6d08570f07b4ef32282e6555d9e3d1c5'
+url =  f'https://api.themoviedb.org/3/movie/11?api_key={API_KEY}'
+
+def extract_film_data(url:str) -> list:
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        print("Erro de requisição")
+        return []
+        
+    data = response.json()
+
+    if not data:
+        print("Dados vazios")
+        return []
+
+    raw_data_path = "../ETLlocal-films/data/raw_latest_films.json"
+
+    f = open(raw_data_path, 'w')
+    json.dump(data, f, indent=4)
+    f.close()
+
+    print(f"Arquivo json cru de ultimos filmes em {raw_data_path}")
+
+    
+    return data
+
+extract_film_data(url)
